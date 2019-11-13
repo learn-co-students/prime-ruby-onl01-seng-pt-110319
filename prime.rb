4b5e3ba03157406fdbbc02c99d7a24f15a75d38f
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-11-13 16:19:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+#what defines an integer as a prime number? Anything => 1 or <= -1. 0 is not a prime number. 
+
+def prime?(num)
+  if num <= 1 || num == 0 || num == 1
+    return false
+  elsif
+    (2..num - 1).each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+  end 
+  true  
+end
