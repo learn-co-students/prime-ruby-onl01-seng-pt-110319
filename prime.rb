651e118452c9a0636df8823d4d441690b7f41e82
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-24 02:10:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+
+
+
+
+
+
+def prime?(n) 
+  return false if n < 2
+  return true if n == 2
+  (2..Math.sqrt(n).ceil).each do |x|
+   return false if n % x == 0
+  end
+  true
+end
